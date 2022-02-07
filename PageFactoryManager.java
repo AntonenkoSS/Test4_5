733d0a0fa93acf707e19017adec4b77477b3c988
Sergii <ssantonenko@gmail.com>
2022-02-07 12:32:34 +0200

    Add files via upload

---
+++ PageFactoryManager.java
@@ -0,0 +1,20 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import pages.*;
+
+public class PageFactoryManager {
+
+    WebDriver driver;
+
+    public PageFactoryManager(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public HomePage getHomePage(){ return new HomePage(driver);}
+
+    public NewsPage getNewsPage(){ return new NewsPage(driver);}
+
+    public SearchResultPage getSearchResultPage(){ return new SearchResultPage(driver);}
+
+}
